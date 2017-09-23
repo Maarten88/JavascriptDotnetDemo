b2ab2f3c5502013e98253db8a0ce9b343c59299d
Maarten Sikkema <Maarten@sikkema.com>
2017-09-23 18:38:25 +0200

    connect hydrate to correct div

--- boot-server.tsx
+++ boot-server.tsx
@@ -5,8 +5,17 @@ import { renderToNodeStream } from 'react-dom/server';
 import { Blog } from './components/blog';
 import { blogs } from './store/blogs';
 import { ReactElement } from 'react';
+import * as stream from "stream";
 
-type RenderToNodeStreamFunc = (element: ReactElement<any>) => any;
+type RenderToNodeStreamFunc = (element: ReactElement<any>) => stream.Readable;
+
+const App = () => {
+    return(
+        <div id="app">
+            <Blog blogs={blogs} />
+        </div>
+    );
+}
 
 export const appFunc = () => {
     const app = express();
@@ -15,16 +24,15 @@ export const appFunc = () => {
 
     app.get('/', function (_, res) { 
 
-        res.write("<!DOCTYPE html><html><head><meta charset=\"utf-8\"><link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css\" integrity=\"sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u\" crossorigin=\"anonymous\"></head><body><div id=\"app\"></div>");
+        res.write("<!DOCTYPE html><html><head><meta charset=\"utf-8\"><link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css\" integrity=\"sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u\" crossorigin=\"anonymous\"></head><body>");
 
-        renderToNodeStream(<Blog blogs={blogs} />)
+        renderToNodeStream(<App />)
             .pipe(res, {end: false})
             .on('unpipe', () => {
                 res.write("<script src=\"dist/client.js\"></script></body></html>");
                 res.end();
-            })
-
-    });
+            });
+});
 
     return app;
 };
